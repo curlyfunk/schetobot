beb7f9fa8d7c339fa2a0e819f62079016fc3308a
Dimi Kostadinov <dimi@dimi.com>
2025-06-01 09:28:25 +0300

    Обновяване на структурата наново

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,3 @@
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -7,19 +6,12 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: {
-          light: '#EDE9F5',
-          DEFAULT: '#6B47DC',
-          dark: '#4C34A0',
-        },
-        gray: {
-          light: '#F8F9FA',
-          DEFAULT: '#6B7280',
-          dark: '#374151',
-        },
+        primary: '#6C3EFF', // основен лилав цвят (базиран на accountable.de)
+        secondary: '#F6F2FF',
+        accent: '#D1B2FF',
       },
       fontFamily: {
-        sans: ['Inter', 'ui-sans-serif', 'system-ui'],
+        sans: ['Inter', 'sans-serif'],
       },
     },
   },
