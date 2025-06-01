a1d3392857708fe96e447ee2ad5a24b258e2cb93
curlyfunk <dimitrifromsofia@gmail.com>
2025-06-01 10:03:19 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,17 @@
-'use client';
-
-import React from 'react';
-import Header from '../components/Header';
-import Hero from '../components/Hero';
-import Features from '../components/Features';
-import HowItWorks from '../components/HowItWorks';
-import InsuranceCalculator from '../components/InsuranceCalculator';
-import TaxCalendar from '../components/TaxCalendar';
-import FAQ from '../components/FAQ';
-import PressSection from '../components/PressSection';
-import PaymentSystem from '../components/PaymentSystem';
-import CallToAction from '../components/CallToAction';
-import AdvancedChatbot from '../components/AdvancedChatbot';
-import Footer from '../components/Footer';
+import Image from "next/image";
+import Hero from "@/components/Hero";
+import Features from "@/components/Features";
+import HowItWorks from "@/components/HowItWorks";
+import Pricing from "@/components/Pricing";
+import FAQ from "@/components/FAQ";
+import Footer from "@/components/Footer";
+import InsuranceCalculator from "@/components/InsuranceCalculator";
+import TaxCalendar from "@/components/TaxCalendar";
+import DocumentGenerator from "@/components/DocumentGenerator";
 
 export default function Home() {
   return (
-    <main className="min-h-screen">
-      <Header />
+    <>
       <Hero />
       <Features />
       <HowItWorks />
@@ -26,11 +20,7 @@ export default function Home() {
       <TaxCalendar />
       <Pricing />
       <FAQ />
-      <PressSection />
-      <PaymentSystem />
-      <CallToAction />
-      <AdvancedChatbot />
       <Footer />
-    </main>
+    </>
   );
 }
